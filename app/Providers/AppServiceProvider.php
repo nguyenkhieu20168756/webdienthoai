ed3565874d0856fffe65508f73bfd068a85f89f1
nguyenkhieu20168756 <nguyen.kv168756@sis.hust.edu.vn>
2021-07-09 20:32:57 +0700

    Add page products

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -32,6 +32,14 @@ class AppServiceProvider extends ServiceProvider
                  ]
             );
         });
+        view()->composer('products',function($view){
+            $view->with(
+                [
+                    'products' => DB::table('products')->where('status','=',1)->get(),
+                    'categories' => DB::table('categories')->where('status','=',1)->get()
+                ]
+           );
+       });
         view()->composer('admin.layouts.index',function($view){
             $data = DB::select(
                 'SELECT DATE_FORMAT(o.created_at,"%d/%m/%Y") order_day,SUM(o.price)-p.price total_price FROM orders o,promotions p WHERE o.promotion_id = p.id and o.status = 2 GROUP BY order_day'
